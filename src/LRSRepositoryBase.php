20686fb2d43927e6456784b3e81dec47d50b7ce2
khoa <khoa@go1.com.au>
2014-10-10 06:33:51 +0000

    implement getStatementById

--- src/LRSRepositoryBase.php
+++ src/LRSRepositoryBase.php
@@ -2,20 +2,33 @@
 
 namespace GO1\Aduro\TinCan;
 
+use GuzzleHttp\ClientInterface;
+
+
 class LRSRepositoryBase implements LRSRepositoryInterface {
+  
+  protected $client;
+  
+  protected $lrs;
+
+  public function __construct(ClientInterface $client, LRS $lrs) {
+    $this->client = $client;
+    $this->lrs = $lrs;
+  }
   
   /**
    * @{inheritdoc}
    */
   public function getStatement($actor, $verbID, $object) {
-    
+   
   }
 
   /**
    * @{inheritdoc}
    */
   public function getStatementById($statementID) {
-    
+    $response = $this->client->get($this->lrs->getEndpoint(), array('statementId' => $statementID));
+    return $response->getBody();
   }
 
   /**
