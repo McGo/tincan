852464528aae9b1e2e6e630bc4b6856e8a3e045e
khoa <khoa@go1.com.au>
2014-10-13 00:13:44 +0000

    fix LRSInterface

--- src/LRSInterface.php
+++ src/LRSInterface.php
@@ -3,12 +3,29 @@
 namespace GO1\Aduro\TinCan;
 
 interface LRSInterface {
-
+  
+  /**
+   * @return string url to LRS endpoint, trailing slash is necessary
+   */
   function getEndpoint();
 
+  /**
+   * Set url of LRS
+   * @param type $endpoint
+   * 
+   */
   function setEndpoint($endpoint);
   
+  /**
+   * @return string HTTP basic access authentication
+   */
   function getAuth();
 
-  function setAuth($password);
+  /**
+   * Set HTTP basic access authentication
+   * @param string $username
+   * @param string $password
+   */
+  function setAuth($username, $password);
+  
 }
