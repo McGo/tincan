70c977fcdecd2c8c9b8c337b1453f4c45a795bca
khoa <khoa@go1.com.au>
2014-10-13 03:37:18 +0000

    parse method of StatementParserInterface

--- src/StatementParserBase.php
+++ src/StatementParserBase.php
@@ -1,8 +1,12 @@
 <?php
+
 namespace GO1\Aduro\TinCan;
-use GO1\Aduro\TinCan\StatementParserInterface;
 
 class StatementParserBase implements StatementParserInterface {
   
+  public function parse($json) {
+    
+  }
+
 }
 
--- src/StatementParserInterface.php
+++ src/StatementParserInterface.php
@@ -1,7 +1,14 @@
 <?php
+
 namespace GO1\Aduro\TinCan;
 
 interface StatementParserInterface {
+  
+  /**
+   * 
+   * @param string $json
+   */
+  function parse($json);
   
 }
 
