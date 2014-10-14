0d86e47e4fd54e108a16132196a800aae86577d9
khoa <khoa@go1.com.au>
2014-10-14 06:37:12 +0000

    mock Client and stub send n createRequest

--- tests/LRSRepositoryBaseTest.php
+++ tests/LRSRepositoryBaseTest.php
@@ -4,114 +4,106 @@ namespace GO1\Aduro\TinCan\Test;
 
 use GO1\Aduro\TinCan\LRSRepositoryBase;
 use GO1\Aduro\TinCan\LRS;
-use GuzzleHttp\Client;
+use GuzzleHttp\Message\Response;
+use GuzzleHttp\Message\Request;
+use GuzzleHttp\Stream\Stream;
 use TinCan\Agent;
 use TinCan\RemoteLRS;
 use TinCan\Activity;
 use GO1\Aduro\TinCan\StatementParserBase;
 
-define('COMMON_EMAIL',       'tincanphp@tincanapi.com');
-define('COMMON_GROUP_EMAIL', 'tincanphp+group@tincanapi.com');
-define('COMMON_MBOX',        'mailto:tincanphp@tincanapi.com');
-define('COMMON_GROUP_MBOX',  'mailto:tincanphp+group@tincanapi.com');
-define('COMMON_VERB_ID',     'http://adlnet.gov/expapi/verbs/experienced');
-define('COMMON_ACTIVITY_ID', 'http://tincanapi.com/TinCanPHP/Test/Activity');
-
 class LRSRepositoryBaseTest extends \PHPUnit_Framework_TestCase {
 
-  static private $endpoint = 'http://duynguyen.lrs.aduro.go1.com.vn/data/xAPI/';
-  static private $username = 'ed0bd8f9ece36054232a66c4386261397b18bdb6';
-  static private $password = 'b5aa8c3732b2a7eb982bb4ff02dacac92a60cd9c';
-  static private $version  = '1.0.1';
-  protected $remoteLRS;
-  protected $lrs;
-  protected $lrsRepo;
-  protected $client;
+  protected $repository;
 
   protected function setUp() {
-    $this->remoteLRS = new RemoteLRS(self::$endpoint, self::$version, self::$username, self::$password);
-    $this->lrs = new LRS(self::$endpoint, self::$username, self::$password);
-    $this->client = new Client(['base_url' => $this->lrs->getEndpoint()]);
-    $this->parse = new StatementParserBase();
-    $this->lrsRepo = new LRSRepositoryBase($this->client, $this->lrs, $this->parse);
+    //mock $client
+    $client = $this->getMockBuilder('GuzzleHttp\Client')
+      ->disableOriginalConstructor()
+      ->getMock();
+    //stub send()
+    $response = new Response(200);
+    $stream = Stream::factory(file_get_contents(__DIR__ . '/fixtures/statement.json'));
+    $stream->read(1);
+    $response->setBody($stream);
+    $client->method('send')->willReturn($response);
+    
+    //stub createRequest()
+    $request = new \GuzzleHttp\Message\Request('GET', 'example.com');
+    $client->method('createRequest')->willReturn($request);
+    
+    $lrs = new LRS('lrs.example.com', 'user', 'password');
+    
+    $parser = new StatementParserBase();
+    
+    $this->repository = new LRSRepositoryBase($client, $lrs, $parser);
   }
   
-  function createStatement($mbox = COMMON_MBOX, $verb = COMMON_VERB_ID, $object = COMMON_ACTIVITY_ID) {
-    $saveResponse = $this->remoteLRS->saveStatement(
-      [
-        'actor' => ['mbox' => $mbox],
-        'verb' => ['id' => $verb, 'display' => ['US' => 'US']],
-        'object' => new Activity(['id' => $object])
-      ]
-    );
-    return $saveResponse->success ? $saveResponse : FALSE;
-  }
-
-  public function testGetStatement() {
-    $mbox = uniqid()  . '@go1.com.au';
-    $verbId = 'http://adlnet.gov/expapi/verbs/' . uniqid();
-    $object = 'http://tincanapi.com/go1/' . uniqid() . '.html';
-    $this->createStatement($mbox, $verbId, $object);
-    
-    $statement  = $this->lrsRepo->getStatement($mbox, $verbId, $object);
-    $this->assertInstanceOf('TinCan\Statement', $statement);
-    $this->assertEquals('mailto:' . $mbox, $statement->getActor()->getMbox());
-    $this->assertEquals($verbId, $statement->getVerb()->getId());
-    $this->assertEquals($object, $statement->getObject()->getId());
-  }
-
-  public function testgGetStatementById() {
-    $saveResponse = $this->createStatement();
-    $statement = $this->lrsRepo->getStatementById($saveResponse->content->getId());
-    $this->assertInstanceOf('TinCan\Statement', $statement);
-  }
-
-  public function testGetStatementsHasActor() {
-    $mbox = uniqid()  . '@go1.com.au';
-    for($i = 0; $i < 3; ++$i) {
-      $this->createStatement($mbox);
-    }
-    
-    // getStatementsHasActor
-    $obj = new Agent([ 'mbox' => $mbox]);
-    $params = array(
-      'objectType' => $obj->getObjectType(),
-      'mbox' => $obj->getMbox(),
-    );
-    $statements = $this->lrsRepo->getStatementsHasActor(json_encode($params));
-    $this->assertEquals(3, count($statements));
-    foreach ($statements as $statement) {
-      $this->assertEquals('mailto:' . $mbox, $statement->getActor()->getMbox());
-      $this->assertInstanceOf('TinCan\Statement', $statement);
-    }
-  }
-
-  public function testGetStatementsHasVerb() {
-    $verbId = 'http://adlnet.gov/expapi/verbs/' . uniqid();
-    for($i = 0; $i < 4; ++$i) {
-      $this->createStatement(COMMON_EMAIL, $verbId);
-    }
-    
-    // getStatementsHasVerb
-    $statements = $this->lrsRepo->getStatementsHasVerb($verbId);
-    $this->assertEquals(4, count($statements));
-    foreach ($statements as $statement) {
-      $this->assertEquals($verbId, $statement->getVerb()->getId());
-      $this->assertInstanceOf('TinCan\Statement', $statement);
-    }
-  }
-
-  public function testGetStatementsHasObject() {
-    $object = 'http://tincanapi.com/go1/' . uniqid() . '.html';
-    for($i = 0; $i < 2; ++$i) {
-      $this->createStatement(COMMON_EMAIL, COMMON_VERB_ID, $object);
-    }
-    $statements = $this->lrsRepo->getStatementsHasObject($object);
-    $this->assertEquals(2, count($statements));
-    foreach ($statements as $statement) {
-      $this->assertEquals($object, $statement->getObject()->getId());
-      $this->assertInstanceOf('TinCan\Statement', $statement);
-    }
-  }
+//  public function testGetStatement() {
+//    $mbox = uniqid()  . '@go1.com.au';
+//    $verbId = 'http://adlnet.gov/expapi/verbs/' . uniqid();
+//    $object = 'http://tincanapi.com/go1/' . uniqid() . '.html';
+//    $this->createStatement($mbox, $verbId, $object);
+//    
+//    $statement  = $this->lrsRepo->getStatement($mbox, $verbId, $object);
+//    $this->assertInstanceOf('TinCan\Statement', $statement);
+//    $this->assertEquals('mailto:' . $mbox, $statement->getActor()->getMbox());
+//    $this->assertEquals($verbId, $statement->getVerb()->getId());
+//    $this->assertEquals($object, $statement->getObject()->getId());
+//  }
+//
+//  public function testgGetStatementById() {
+//    $saveResponse = $this->createStatement();
+//    $statement = $this->lrsRepo->getStatementById($saveResponse->content->getId());
+//    $this->assertInstanceOf('TinCan\Statement', $statement);
+//  }
+//
+//  public function testGetStatementsHasActor() {
+//    $mbox = uniqid()  . '@go1.com.au';
+//    for($i = 0; $i < 3; ++$i) {
+//      $this->createStatement($mbox);
+//    }
+//    
+//    // getStatementsHasActor
+//    $obj = new Agent([ 'mbox' => $mbox]);
+//    $params = array(
+//      'objectType' => $obj->getObjectType(),
+//      'mbox' => $obj->getMbox(),
+//    );
+//    $statements = $this->lrsRepo->getStatementsHasActor(json_encode($params));
+//    $this->assertEquals(3, count($statements));
+//    foreach ($statements as $statement) {
+//      $this->assertEquals('mailto:' . $mbox, $statement->getActor()->getMbox());
+//      $this->assertInstanceOf('TinCan\Statement', $statement);
+//    }
+//  }
+//
+//  public function testGetStatementsHasVerb() {
+//    $verbId = 'http://adlnet.gov/expapi/verbs/' . uniqid();
+//    for($i = 0; $i < 4; ++$i) {
+//      $this->createStatement(COMMON_EMAIL, $verbId);
+//    }
+//    
+//    // getStatementsHasVerb
+//    $statements = $this->lrsRepo->getStatementsHasVerb($verbId);
+//    $this->assertEquals(4, count($statements));
+//    foreach ($statements as $statement) {
+//      $this->assertEquals($verbId, $statement->getVerb()->getId());
+//      $this->assertInstanceOf('TinCan\Statement', $statement);
+//    }
+//  }
+//
+//  public function testGetStatementsHasObject() {
+//    $object = 'http://tincanapi.com/go1/' . uniqid() . '.html';
+//    for($i = 0; $i < 2; ++$i) {
+//      $this->createStatement(COMMON_EMAIL, COMMON_VERB_ID, $object);
+//    }
+//    $statements = $this->lrsRepo->getStatementsHasObject($object);
+//    $this->assertEquals(2, count($statements));
+//    foreach ($statements as $statement) {
+//      $this->assertEquals($object, $statement->getObject()->getId());
+//      $this->assertInstanceOf('TinCan\Statement', $statement);
+//    }
+//  }
 
 }
